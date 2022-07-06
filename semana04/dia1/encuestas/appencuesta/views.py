b1a333aed09c93fe13d1df2349633d454627fdfa
PeterArce96 <parce0496@gmail.com>
2022-07-05 23:52:48 -0500

    semana04 - dia1

---
+++ semana04/dia1/encuestas/appencuesta/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def mostrar_encuesta(request):
+    # return HttpResponse('<h1>Encuesta Nro 1</h1>')
+    return render(request,'index.html')
+
+def pregunta(request,pregunta_id):
+    return HttpResponse('<h2>Pregunta Nro° %s</h2>' % pregunta_id)
+
+def enviar_resultado(request):
+    nombre = request.POST['nombre']
+    rol = request.POST['rol']
+    idiomas = request.POST.getlist('idiomas')
+
+    context = {
+        'nombre':nombre,
+        'rol':rol,
+        'idiomas':idiomas
+    }
+
+    return render(request,'respuesta.html',context)
