9e53e912f0787f3e1591262d8b4d9fe71329a1e6
PeterArce96 <parce0496@gmail.com>
2022-07-11 20:48:04 -0500

    get, post - alumnos en drf

--- semana05/dia1/alumnos/api/admin.py
+++ semana05/dia1/alumnos/api/admin.py
@@ -1,3 +1,8 @@
+import imp
+from multiprocessing.spawn import import_main_path
 from django.contrib import admin
 
 # Register your models here.
+from .models import Alumno
+
+admin.site.register(Alumno)
---
+++ semana05/dia1/alumnos/api/urls.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index),
+    path('alumno',views.alumnos),
+    path('home',views.home),
+    path('getalumno',views.getAlumnos),
+    path('setalumno',views.setAlumno)
+]
