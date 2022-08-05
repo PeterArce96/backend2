a15c75174c60c2be3e51a5c060327e8ddf133c8a
PeterArce96 <parce0496@gmail.com>
2022-08-04 22:02:26 -0500

    api rest de cursos

--- semana08/dia2/index.js
+++ semana08/dia2/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+
 const mysqlConnection = require('./database');
 
 const app = express();
 const port = 5000;
+
 
 //permite que el servidor reciba data en json
 app.use(express.json());
@@ -31,7 +33,7 @@ app.post('/alumno',(req,res)=>{
     const {nombre,email} = req.body;
 
     const query = `insert into tbl_alumno(alumno_nombre,alumno_email)
-                   values('${nombre}','${email}')`;
+                    values('${nombre}','${email}')`;
     
     mysqlConnection.query(query,(err,rows,fields)=>{
         if(!err){
@@ -75,6 +77,72 @@ app.delete('/alumno/:id',(req,res)=>{
                 res.json({
                     'status':true,
                     'content':'alumno eliminado'
+                })
+            }else{
+                console.log(err);
+            }
+        })
+})
+
+//api rest de cursos
+app.get('/curso',(req,res)=>{
+    mysqlConnection.query('select * from tbl_curso',(err,rows,fields)=>{
+        if(!err){
+            res.json(rows);
+        }else{
+            console.log(err);
+        }
+    })
+})
+
+app.post('/curso',(req,res)=>{
+    const {nombre} = req.body;
+
+    const query = `insert into tbl_curso(curso_nombre)
+                    values('${nombre}')`;
+    
+    mysqlConnection.query(query,(err,rows,fields)=>{
+        if(!err){
+            res.json({
+                'status':true,
+                'content':'curso creado'
+            })
+        }else{
+            console.log(err);
+        }
+    });
+});
+
+app.put('/curso/:id',(req,res)=>{
+    const {nombre} = req.body;
+    const {id} = req.params;
+
+    const query = "update tbl_curso set curso_nombre=? where curso_id=?";
+
+    mysqlConnection.query(query,[nombre,id],
+        (err,rows,fields)=>{
+            if(!err){
+                res.json({
+                    'status':true,
+                    'content':'curso actualizado'
+                })
+            }else{
+                console.log(err);
+            }
+        })
+})
+
+app.delete('/curso/:id',(req,res)=>{
+    const {id} = req.params;
+
+    query = "delete from tbl_curso where curso_id = ?"
+
+    mysqlConnection.query(query,[id],
+        (err,rows,fields)=>{
+            if(!err){
+                res.json({
+                    'status':true,
+                    'content':'curso eliminado'
                 })
             }else{
                 console.log(err);
