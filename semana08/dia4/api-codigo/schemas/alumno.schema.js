5f9d65f6ac21efd03c49a54824a6c6ab8dd3e9f7
PeterArce96 <parce0496@gmail.com>
2022-08-09 19:09:27 -0500

    dia4 terminado

---
+++ semana08/dia4/api-codigo/schemas/alumno.schema.js
@@ -0,0 +1,15 @@
+const Joi = require('joi');
+
+const nombre = Joi.string().min(3).max(100);
+const email = Joi.string().email();
+
+const createAlumnoSchema = Joi.object({
+    nombre: nombre.required(),
+    email: email.required()
+});
+
+const updateAlumnoSchema = Joi.object({
+    nombre: nombre.required()
+})
+
+module.exports = {createAlumnoSchema,updateAlumnoSchema}
