740d879b814a0009e5e3b61b84a90807767103b3
PeterArce96 <parce0496@gmail.com>
2022-08-11 21:07:46 -0500

    agregar tarea

---
+++ semana09/dia2/backend/src/controllers/tarea.controller.js
@@ -0,0 +1,19 @@
+const tareaController = {};
+
+const tareaModel = require('../models/tarea.model');
+
+tareaController.getAll = async (req,res)=>{
+    const tareas = await tareaModel.find();
+    res.json(tareas);
+}
+tareaController.create = async(req,res)=>{
+    const {nombre,estado} = req.body;
+    const nuevaTarea = new tareaModel({
+        nombre,
+        estado
+    })
+    await nuevaTarea.save();
+    res.json(nuevaTarea);
+}
+
+module.exports = tareaController;
