353c5a507795943bb8bf61bdfb031533e44ff625
PeterArce96 <parce0496@gmail.com>
2022-08-23 21:33:50 -0500

    perudemy api

---
+++ perudemy/backend/apimiembros/api/views.py
@@ -0,0 +1,25 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+
+from .models import *
+from .serializers import *
+
+class IndexView(APIView):
+
+    def get(self,request):
+        context = {
+            'status':True,
+            'message':'api de miembros listo'
+        }
+
+        return Response(context)
+
+# class CategoriaView(generics.ListAPIView):
+#     queryset = Categoria.objects.all()
+#     serializer_class = CategoriaSerializer
+
+
+# class CursoView(generics.ListAPIView):
+#     queryset = Curso.objects.all()
+#     serializer_class = CursoSerializer
